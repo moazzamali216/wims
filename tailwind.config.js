e6b063a66c5188ec5ef69a77c5d94f55c3e3ad3a
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2025-10-25 01:26:21 +0500

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./public/**/*.html"],
+  content: ["./**/*.html","./index.html","./public/**/*.html",],
   theme: {
     extend: {},
   },
